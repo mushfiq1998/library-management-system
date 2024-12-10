f03ab5b6a0d336d21fc37f942155f413c407446a
mushfiq1998 <kazimushfiqcse@gmail.com>
2024-12-10 21:23:04 +0600

    Prepare for production

--- libraryPro/settings.py
+++ libraryPro/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(=4=3x#d-r2#iyecy-vu=%%a9v*_z3rjsj&sb09ui#et7laiyv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Mushfiq3.pythonanywhere.com']
 
 
 # Application definition
